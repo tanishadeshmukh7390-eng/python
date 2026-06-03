9bb7db61ec5a4036c57f21626b55cceadc6033b1
tanisha <tanishadeshmukh739o@gmail.com>
2026-06-03 21:02:55 +0530

    Day 1: first program

---
+++ variables.py
@@ -0,0 +1,8 @@
+# A variable stores a variable
+student_name ="tanisha" 
+marks = 93
+is_passed =True
+
+print(student_name)      #outout:tanisha 
+print(marks)             #output:93
+print(is_passed)         #output:true
